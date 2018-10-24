6fb587983f40a5a4b6be65d54c8dbd1cc21e5cb1
Naeblis <johnmlowery@gmail.com>
2018-10-24 11:58:32 -0500

    all the stuff

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,11 +1,18 @@
 class PetsController < ApplicationController
 
     def index
-        render json: Pet.all
+        pets = Pet.all
+        pets.each do |pet|
+            pet.likes = pet.liker_pets.length
+            pet.save
+        end
+        render json: pets
     end
 
     def show
-        render json: Pet.find(params[:id])
+        pet = Pet.find(params[:id])
+        likes = pet.liker_pets.length
+        render json: {likes: likes, pet: pet}
     end
 
     def create
@@ -14,10 +21,23 @@ class PetsController < ApplicationController
     end
 
     def update
+
+        # create no like using liker_id and liked_id
+        like = Like.create(liker_id: params[:liker_id], liked_id: params[:liked_id])
+        # byebug
+        # number of likers for liked pet
+        number_of_likes = Pet.find(params[:liked_id]).liker_pets.length
+        
+        if like.save
+            render json: {likes: number_of_likes}
+        else
+            render json: like.errors, status: :bad_request
+        end
     end
 
     def destroy
     end
+
 
     private
 
